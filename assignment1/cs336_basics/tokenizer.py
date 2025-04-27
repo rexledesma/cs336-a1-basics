d5c5df2e25a2719de8bda8cb5417fd43b31ac877
Rex Ledesma <rex@rexledesma.com>
2025-04-26 20:08:50 -0400

    feat(bpe): bring back simplified merge

--- assignment1/cs336_basics/tokenizer.py
+++ assignment1/cs336_basics/tokenizer.py
@@ -6,58 +6,82 @@ import regex
 GPT2_TOKENIZER_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 
-def generate_positions_to_merge(pre_token: tuple[bytes, ...], most_frequent_pair: tuple[bytes, bytes]) -> list[int]:
-    return [position for position, pair in enumerate(zip(pre_token, pre_token[1:])) if pair == most_frequent_pair]
+# def generate_positions_to_merge(pre_token: tuple[bytes, ...], most_frequent_pair: tuple[bytes, bytes]) -> list[int]:
+#     return [position for position, pair in enumerate(zip(pre_token, pre_token[1:])) if pair == most_frequent_pair]
+
+
+# def merge(
+#     frequencies_for_pre_token: dict[tuple[bytes, ...], int],
+#     frequencies_for_pair: dict[tuple[bytes, bytes], int],
+#     most_frequent_pair: tuple[bytes, bytes],
+#     new_index: int,
+# ) -> tuple[
+#     dict[tuple[bytes, ...], int],
+#     dict[tuple[bytes, bytes], int],
+# ]:
+#     new_frequencies_for_pre_token = {}
+
+#     for pre_token, frequency in frequencies_for_pre_token.items():
+#         new_pre_token = []
+#         positions_to_merge = set(generate_positions_to_merge(pre_token, most_frequent_pair))
+
+#         jdx = 0
+#         while jdx < len(pre_token):
+#             if jdx in positions_to_merge:
+#                 # Decrement frequencies for pairs being replaced
+#                 if jdx > 0:
+#                     prev_pair = (pre_token[jdx - 1], pre_token[jdx])
+#                     frequencies_for_pair[prev_pair] -= frequency
+#                 if jdx < len(pre_token) - 2:
+#                     next_pair = (pre_token[jdx + 1], pre_token[jdx + 2])
+#                     frequencies_for_pair[next_pair] -= frequency
+
+#                 # Add the merged token
+#                 new_pre_token.append(new_index)
+#                 jdx += 2
+
+#                 # Increment frequencies for new pairs
+#                 if len(new_pre_token) > 1:
+#                     new_pair = new_pre_token[-2], new_pre_token[-1]
+#                     frequencies_for_pair[new_pair] = frequencies_for_pair.get(new_pair, 0) + frequency
+
+#                 if jdx < len(pre_token):
+#                     new_pair = new_pre_token[-1], pre_token[jdx]
+#                     frequencies_for_pair[new_pair] = frequencies_for_pair.get(new_pair, 0) + frequency
+#             else:
+#                 new_pre_token.append(pre_token[jdx])
+
+#                 jdx += 1
+
+#         new_frequencies_for_pre_token[tuple(new_pre_token)] = frequency
+
+#     frequencies_for_pair.pop(most_frequent_pair, None)
+
+#     return new_frequencies_for_pre_token, frequencies_for_pair
 
 
 def merge(
     frequencies_for_pre_token: dict[tuple[bytes, ...], int],
-    frequencies_for_pair: dict[tuple[bytes, bytes], int],
     most_frequent_pair: tuple[bytes, bytes],
-    new_index: int,
-) -> tuple[
-    dict[tuple[bytes, ...], int],
-    dict[tuple[bytes, bytes], int],
-]:
+) -> dict[tuple[bytes, ...], int]:
     new_frequencies_for_pre_token = {}
 
     for pre_token, frequency in frequencies_for_pre_token.items():
         new_pre_token = []
-        positions_to_merge = set(generate_positions_to_merge(pre_token, most_frequent_pair))
-
-        jdx = 0
-        while jdx < len(pre_token):
-            if jdx in positions_to_merge:
-                # Decrement frequencies for pairs being replaced
-                if jdx > 0:
-                    prev_pair = (pre_token[jdx - 1], pre_token[jdx])
-                    frequencies_for_pair[prev_pair] -= frequency
-                if jdx < len(pre_token) - 2:
-                    next_pair = (pre_token[jdx + 1], pre_token[jdx + 2])
-                    frequencies_for_pair[next_pair] -= frequency
-
-                # Add the merged token
-                new_pre_token.append(new_index)
-                jdx += 2
-
-                # Increment frequencies for new pairs
-                if len(new_pre_token) > 1:
-                    new_pair = new_pre_token[-2], new_pre_token[-1]
-                    frequencies_for_pair[new_pair] = frequencies_for_pair.get(new_pair, 0) + frequency
-
-                if jdx < len(pre_token):
-                    new_pair = new_pre_token[-1], pre_token[jdx]
-                    frequencies_for_pair[new_pair] = frequencies_for_pair.get(new_pair, 0) + frequency
+        idx = 0
+        while idx < len(pre_token):
+            if idx < len(pre_token) - 1 and (pre_token[idx], pre_token[idx + 1]) == most_frequent_pair:
+                # Merge the pair
+                new_pre_token.append(most_frequent_pair[0] + most_frequent_pair[1])
+                idx += 2
             else:
-                new_pre_token.append(pre_token[jdx])
+                new_pre_token.append(pre_token[idx])
+                idx += 1
 
-                jdx += 1
+        new_pre_token = tuple(new_pre_token)
+        new_frequencies_for_pre_token[new_pre_token] = new_frequencies_for_pre_token.get(new_pre_token, 0) + frequency
 
-        new_frequencies_for_pre_token[tuple(new_pre_token)] = frequency
-
-    frequencies_for_pair.pop(most_frequent_pair, None)
-
-    return new_frequencies_for_pre_token, frequencies_for_pair
+    return new_frequencies_for_pre_token
 
 
 def pre_tokenize(input_path: str | os.PathLike, special_tokens: list[str]) -> dict[tuple[bytes, ...], int]:
@@ -134,12 +158,12 @@ def train_bpe_tokenizer(
     # Count the number of occurences for each pair of tokens
     frequencies_for_pre_token = pre_tokenize(input_path, special_tokens)
 
-    # Get the frequency of pairs
-    frequencies_for_pairs = build_pair_frequencies(frequencies_for_pre_token)
-
     # Add special tokens to the vocabulary
     num_merges = vocab_size - len(vocabulary_for_index)
     for _ in range(num_merges):
+        # Get the frequency of pairs
+        frequencies_for_pairs = build_pair_frequencies(frequencies_for_pre_token)
+
         # Find most common pair
         most_frequent_pair = max(frequencies_for_pairs, key=lambda p: (frequencies_for_pairs[p], p))
 
@@ -148,8 +172,6 @@ def train_bpe_tokenizer(
         index_pair_merges.append(most_frequent_pair)
         vocabulary_for_index[new_index] = most_frequent_pair[0] + most_frequent_pair[1]
 
-        frequencies_for_pre_token, frequencies_for_pairs = merge(
-            frequencies_for_pre_token, frequencies_for_pairs, most_frequent_pair, new_index
-        )
+        frequencies_for_pre_token = merge(frequencies_for_pre_token, most_frequent_pair)
 
     return vocabulary_for_index, index_pair_merges
