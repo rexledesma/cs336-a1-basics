f906ac58eb5ca453b69518e851e625274c8f067a
Rex Ledesma <rex@rexledesma.com>
2025-06-04 12:09:10 -0400

    chore(bpe): add script to test against TinyStories

---
+++ assignment1/cs336_basics/tiny_stories.py
@@ -0,0 +1,24 @@
+import os
+import pathlib
+import time
+
+from cs336_basics.train_bpe import train_bpe
+
+TINY_STORIES_PATH = (
+    pathlib.Path(__file__)
+    .joinpath("..", "data", "TinyStoriesV2-GPT4-train.txt")
+    .resolve()
+)
+MAX_VOCAB_SIZE = 10_000
+
+if __name__ == "__main__":
+    # Enable multiprocessing for BPE training
+    os.environ["CS336_BPE_ENABLE_MULTIPROCESSING"] = "1"
+
+    start = time.perf_counter()
+    vocabulary, merges = train_bpe(TINY_STORIES_PATH, MAX_VOCAB_SIZE, ["<|endoftext|>"])
+    duration = time.perf_counter() - start
+
+    print(f"Vocabulary size: {len(vocabulary)}")
+    print(f"Merges tail: {merges[-10:]}")
+    print(f"Training took {duration:.2f} seconds")
