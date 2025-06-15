4e0f3f3beace11b7a4206afe907749a14c26cc2e
Rex Ledesma <rex@rexledesma.com>
2025-06-15 12:25:43 -0400

    feat(optimizer): implement AdamW

--- assignment1/cs336_basics/optimizer.py
+++ assignment1/cs336_basics/optimizer.py
@@ -1,3 +1,6 @@
+import math
+from collections.abc import Callable
+
 import torch
 
 
@@ -15,3 +18,46 @@ def cross_entropy(inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     loss = -torch.mean(target_log_probs)
 
     return loss
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, lr: float, weight_decay: float, betas: tuple[float, float], eps: float) -> None:
+        defaults = {
+            "lr": lr,
+            "weight_decay": weight_decay,
+            "betas": betas,
+            "eps": eps,
+        }
+        super().__init__(params, defaults)
+
+    def step(self, closure: Callable[[], float] | None = None) -> float | None:
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            b1, b2 = group["betas"]
+            weight_decay = group["weight_decay"]
+            eps = group["eps"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                state = self.state[p]
+
+                t = state.get("t", 1)
+                m = state.get("m", torch.zeros_like(p.data))
+                v = state.get("v", torch.zeros_like(p.data))
+                grad = p.grad.data
+
+                m = b1 * m + (1 - b1) * grad
+                v = b2 * v + (1 - b2) * torch.pow(grad, 2)
+                lr_t = lr * math.sqrt(1 - b2**t) / (1 - b1**t)
+
+                p.data -= lr_t * m / (torch.sqrt(v) + eps)
+                p.data -= lr * weight_decay * p.data
+
+                state["t"] = t + 1
+                state["m"] = m
+                state["v"] = v
+
+        return loss
--- assignment1/tests/adapters.py
+++ assignment1/tests/adapters.py
@@ -9,7 +9,7 @@ import torch
 from jaxtyping import Float, Int
 from torch import Tensor
 
-from cs336_basics.optimizer import cross_entropy
+from cs336_basics.optimizer import AdamW, cross_entropy
 from cs336_basics.tokenizer import BPETokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.transformer import (
@@ -537,7 +537,7 @@ def get_adamw_cls() -> type[torch.optim.Optimizer]:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
